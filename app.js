f03d501ba592cdf384681784d9909585cb3f680f
Sharath2809 <3dcubestudios@gmail.com>
2020-07-25 13:15:22 +0530

    second commit

---
+++ app.js
@@ -0,0 +1,29 @@
+alert("Hello From Sharath S!");
+var pics = [
+	"images/a.jpg",
+	"images/bhata.jpg",
+	"images/bhoomi.jpg",
+	"images/bhyresh.jpg",
+	"images/deepu.jpg",
+	"images/kd.jpg",
+	"images/megha.jpg",
+	"images/nami.jpg",
+	"images/nis.jpg",
+	"images/pp.jpg",
+	"images/shal.jpg",
+	"images/sid.jpg",
+	"images/sp.jpg",
+	"images/zeesh.jpg"
+] 
+
+var btn = document.querySelector("button");
+var img = document.querySelector("img");
+var counter = 1;
+
+btn.addEventListener("click", function(){
+	if(counter === 14){
+		counter = 0;
+	}
+	img.src = pics[counter]
+	counter = counter + 1;
+});
